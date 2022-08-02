ca12fcdf1a08188a9414b4a0a93075bc492ea88e
unknown <sjasvin9493@gmail.com>
2022-08-02 18:17:26 +0530

    second commit port number update in index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 //setting the port number
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 //setting the path for the views 'ejs'
 const path = require('path');
